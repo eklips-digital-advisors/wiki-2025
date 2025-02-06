6a93bed48a6bacd6cc5c5d44d1b0b1bfb91a23d8
Madis Luik <madis@eklips.se>
2025-02-06 10:33:00 +0200

    Add dynamic image remote patterns based on server URL configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  images: {
+    remotePatterns: [
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
+        const url = new URL(item)
+
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(':', ''),
+        }
+      }),
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
