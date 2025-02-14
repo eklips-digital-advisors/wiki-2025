64fd672281f9133ac297b415707d8a73dc8b5da4
Madis Luik <madis@eklips.se>
2025-02-14 13:39:13 +0200

    Enhance sidebar with collapsible categories and improved UI elements.

--- src/Sidebar/index.tsx
+++ src/Sidebar/index.tsx
@@ -1,63 +1,78 @@
 'use client'
 
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import type { Sidebar as SidebarType } from '@/payload-types'
+import { ChevronRight } from 'lucide-react'
 
 export const SidebarClient: React.FC<{ sidebarPosts: SidebarType }> = ({ sidebarPosts }) => {
   const pathname = usePathname()
+  const [openCategories, setOpenCategories] = useState<number[]>([0]) // First category is open
+
+  const toggleCategory = (index: number) => {
+    setOpenCategories((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index],
+    )
+  }
 
   const getCurrentClass = (slug: string) => {
     return slug.split('/').pop() === pathname.split('/').pop()
       ? 'border-emerald-500 font-medium'
       : 'border-zinc-900/10'
   }
-  
+
   return (
     <div className="px-4 lg:px-0 mt-32 lg:mt-10">
-      {sidebarPosts?.items && sidebarPosts?.items?.map((cat: any, i) => {
-        return (
-          <div key={i} className="relative mt-6">
-            <motion.h2
-              layout="position"
-              className="text-xs font-semibold text-zinc-900"
-            >
-              {cat?.categoriesOrder?.title}
-            </motion.h2>
-            <div className="relative mt-3 pl-2">
-              <ul role="list" className="border-l border-transparent">
-                {cat?.postsOrder && cat?.postsOrder.map((post: any) => {
-                  if (!post?.id) return null
-                  return (
-                    <li className="relative" key={post?.id}>
-                      <Link
-                        className={`${getCurrentClass(post?.slug)} flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 border-l hover:border-emerald-500`}
-                        href={`/posts/${post.slug}`}
-                      >
-                        <span className="truncate">{post.title}</span>
-                      </Link>
-                      {post?.sections &&
-                        post?.slug.split('/').pop() === pathname.split('/').pop() &&
-                        post?.sections?.length > 0 &&
-                        post?.sections.map((section: any, index: number) => (
+      {sidebarPosts?.items &&
+        sidebarPosts?.items?.map((cat: any, i) => {
+          return (
+            <div key={i} className="relative mt-6">
+              <h2
+                className="text-xs font-semibold text-zinc-900 flex gap-2 justify-between items-center cursor-pointer"
+                onClick={() => toggleCategory(i)}
+              >
+                {cat?.categoriesOrder?.title}
+                <ChevronRight
+                  className={`w-4 h-4 transition duration-300 ease-in-out ${openCategories.includes(i) ? 'rotate-90' : 'rotate-0'}`}
+                />
+              </h2>
+
+              <div className={`relative mt-3 pl-2 ${openCategories.includes(i) ? 'visible' : 'hidden'}`}>
+                <ul role="list" className="border-l border-transparent">
+                  {cat?.postsOrder &&
+                    cat?.postsOrder.map((post: any) => {
+                      if (!post?.id) return null
+                      return (
+                        <li className="relative" key={post?.id}>
                           <Link
-                            key={index}
-                            className={`${getCurrentClass(post?.slug)} flex justify-between gap-2 pl-6 py-1 pr-3 text-[12px] transition text-zinc-600 hover:text-zinc-900 border-l hover:border-emerald-500`}
-                            href={`/posts/${post.slug}#section-${index}`}
+                            className={`${getCurrentClass(post?.slug)} flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 hover:text-zinc-900 border-l hover:border-emerald-500`}
+                            href={`/posts/${post.slug}`}
                           >
-                            <span className="truncate"><span className="text-emerald-500">#</span> {section?.sectionName}</span>
+                            <span className="truncate">{post.title}</span>
                           </Link>
-                        ))}
-                    </li>
-                  )
-                })}
-              </ul>
+                          {post?.sections &&
+                            post?.slug.split('/').pop() === pathname.split('/').pop() &&
+                            post?.sections?.length > 0 &&
+                            post?.sections.map((section: any, index: number) => (
+                              <Link
+                                key={index}
+                                className={`${getCurrentClass(post?.slug)} flex justify-between gap-2 pl-6 py-1 pr-3 text-[12px] transition text-zinc-600 hover:text-zinc-900 border-l hover:border-emerald-500`}
+                                href={`/posts/${post.slug}#section-${index}`}
+                              >
+                                <span className="truncate">
+                                  <span className="text-emerald-500">#</span> {section?.sectionName}
+                                </span>
+                              </Link>
+                            ))}
+                        </li>
+                      )
+                    })}
+                </ul>
+              </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
     </div>
   )
 }
