0354f9be14268fc49d61c9200be66ce332c3530c
Madis Luik <madis@eklips.se>
2025-02-19 15:47:31 +0200

    Increase promise delay from 500ms to 2000ms for better reliability.

--- src/blocks/SitesBlock/Component.tsx
+++ src/blocks/SitesBlock/Component.tsx
@@ -81,24 +81,24 @@ export const SitesBlock: React.FC = async () => {
           if (siteIntgrationRepository) {
             const twoFaPath = `repositories/${siteIntgrationRepository}/node.json?path=wp-content/plugins/eklips-2fa`
             twoFaExists = await getSingleRepo(twoFaPath)
-            await promiseDelay(500)
+            await promiseDelay(2000)
 
             const hiddenLoginPath = `repositories/${siteIntgrationRepository}/node.json?path=wp-content/plugins/wps-hide-login`
             hiddenLoginExists = await getSingleRepo(hiddenLoginPath)
-            await promiseDelay(500)
+            await promiseDelay(2000)
 
             const cwaasPath = `repositories/${siteIntgrationRepository}/node.json?path=wp-content/themes/cwaas`
             isCwaas = await getSingleRepo(cwaasPath) ? 'CWAAS' : null
-            await promiseDelay(500)
+            await promiseDelay(2000)
 
             const loadPhpPath = `repositories/${siteIntgrationRepository}/node.json?path=wp-content/themes/cwaas/framework/load.php&contents=true`
             const loadPhp = await getSingleRepo(loadPhpPath)
             if (loadPhp && loadPhp?.contents.includes('box-solr/solr.php')) hasSolr = true
-            await promiseDelay(500)
+            await promiseDelay(2000)
 
             const versionPath = `repositories/${siteIntgrationRepository}/node.json?path=wp-includes/version.php&contents=true`
             const singleRepoWpVersion = await getSingleRepo(versionPath)
-            await promiseDelay(500)
+            await promiseDelay(2000)
 
             if (singleRepoWpVersion && singleRepoWpVersion.contents) {
                 const match = singleRepoWpVersion.contents.match(/\$wp_version\s*=\s*'([^']+)'/);
--- src/utilities/promiseDelay.ts
+++ src/utilities/promiseDelay.ts
@@ -1,3 +1,4 @@
 export function promiseDelay(ms: number) {
+  console.log(`delay ${ms}ms`);
   return new Promise(resolve => setTimeout(resolve, ms));
 }
