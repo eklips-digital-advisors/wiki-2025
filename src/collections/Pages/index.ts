1e13e9bcb6621a125c9abf517cfaeeb69eed8e36
Madis Luik <madis.luik@outlook.com>
2026-01-08 20:21:20 +0200

    Increase autosave interval for drafts in Pages and Posts to 800ms for improved performance.

--- src/collections/Pages/index.ts
+++ src/collections/Pages/index.ts
@@ -110,7 +110,7 @@ export const Pages: CollectionConfig<'pages'> = {
   versions: {
     drafts: {
       autosave: {
-        interval: 100, // We set this interval for optimal live preview
+        interval: 800, // We set this interval for optimal live preview
       },
       schedulePublish: true,
     },
